1faa802d0a3e40d499852d67f5fc0ff4967cb181
VensonLiou <zxc1234529@gmail.com>
2019-04-04 18:31:55 +0800

    Initial Commit

---
+++ TimeTickTicksAway/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTickTicksAway
+//
+//  Created by 劉玟慶 on 2019/4/4.
+//  Copyright © 2019 劉玟慶. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
